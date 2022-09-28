b1f35a4157ee8710bd8018ac4d3c9f6bd9071d17
Matt Crowe <birdman@Matts-MacBook-Pro.local>
2022-09-28 12:06:41 +0300

    Trying one more time

--- index.ts
+++ index.ts
@@ -82,11 +82,11 @@ const textEventHandler = async (event: WebhookEvent): Promise<MessageAPIResponse
     fetch('https://quotes.toscrape.com/random')
     .then(response => {
       for(const pair of response.headers){
-          console.log(`${pair[0]}: ${pair[1]}`); 
+          reply(`${pair[0]}: ${pair[1]}`); 
         }
       return response.text();
   }).then(data => {
-      console.log(data);
+      reply(data);
   });
   };
 
