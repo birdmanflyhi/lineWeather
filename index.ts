ef92e3901fa177e48bd39cbc787e9e413d49a4e8
Matt Crowe <birdman@Matts-MacBook-Pro.local>
2022-09-30 09:20:49 +0300

    Weather fetch attempt

--- index.ts
+++ index.ts
@@ -90,9 +90,14 @@ const textEventHandler = async (event: WebhookEvent): Promise<MessageAPIResponse
   };
 
   async function weatherRequestStandard(){
-   reply('Working on it-Matt');
+   //reply('Working on it-Matt');
+   fetch('https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,daily&units=imperial&appid=${apiKey}')
+   .then((response) => response.json())
+   .then((data) => reply(data));
+      
+   };
 
-  };
+  
 
 async function reply(sendThis:any){
   const response: TextMessage = {
