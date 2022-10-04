1efd02fd616be30b33629f4e566dbbe9d31e165f
Matt Crowe <birdman@Matts-MacBook-Pro.local>
2022-10-04 13:24:36 +0200

    weather current reply

--- index.ts
+++ index.ts
@@ -98,7 +98,7 @@ const textEventHandler = async (event: WebhookEvent): Promise<MessageAPIResponse
    fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,daily&units=imperial&appid=${apiKey}`)
    .then((response) => response.json())
    .then((data) => {
-     console.log( typeof JSON.stringify(data))
+     reply(JSON.stringify(data.weather.current))
      //reply(data.hourly);
    })
    
