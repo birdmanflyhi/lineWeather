3c23d538bb200a17f445c592f2ad49143c48698b
Matt Crowe <birdman@Matts-MacBook-Pro.local>
2022-10-04 12:53:14 +0200

    data to variable

--- index.ts
+++ index.ts
@@ -97,8 +97,8 @@ const textEventHandler = async (event: WebhookEvent): Promise<MessageAPIResponse
    
    fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,daily&units=imperial&appid=${apiKey}`)
    .then((response) => response.json())
-   .then((data) => (hourly = (data)))
-   reply(hourly);
+   .then((data) => reply(hourly = (data)))
+   
     
    
     
