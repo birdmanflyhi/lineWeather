64e51196bd004556cf7a47859845f1506d3250df
Matt Crowe <birdman@Matts-MacBook-Pro.local>
2022-10-20 00:44:32 +0200

    working with backtick

--- index.ts
+++ index.ts
@@ -248,8 +248,8 @@ if(( isNaN(parseInt(text)) == false  && text.length == 5))
            break;
          case 'weather new location':
          case 'new zip code':
-         case "i'm not at home":
-         case "i'm not at that location":
+         case `i'm not at home`:
+         case `i'm not at that location`:
          case 'that is not my address':
          case 'different location':
          case 'new location':
