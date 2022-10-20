21a515ceef061244172fe4d587ca766585b2438b
Matt Crowe <birdman@Matts-MacBook-Pro.local>
2022-10-20 21:03:08 +0200

    did not work switch back

--- index.ts
+++ index.ts
@@ -212,10 +212,6 @@ const textEventHandler = async (event: WebhookEvent): Promise<MessageAPIResponse
 
     }
     reply(response.join("\n"));
-    var randomIndex = Math.floor(Math.random() * motivateQuotes.length);
-    var quote = motivateQuotes[randomIndex];
-    reply(quote);
-   
     
     }); 
    
