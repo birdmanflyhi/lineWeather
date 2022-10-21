6bd245b19826177c76913d070822249f7d75e4eb
Matt Crowe <birdman@Matts-MacBook-Pro.local>
2022-10-21 23:45:54 +0200

    trying day on multiple line

--- index.ts
+++ index.ts
@@ -134,7 +134,8 @@ const textEventHandler = async (event: WebhookEvent): Promise<MessageAPIResponse
        if(dayTime == 'day'){
         if (hour == '9 PM'){
           break;
-        }else if(hour.match(/^(6 AM|7 AM|8 AM|9 AM|10 AM|11 AM|12 PM |1 PM|2 PM|3 PM|4 PM|5 PM|6 PM|7 PM|8 PM|)$/)){
+        }else if(hour.match('/^(6 AM|7 AM|8 AM|9 AM|10 AM|11 AM|12 PM |1 PM|2 PM|'+
+        '3 PM|4 PM|5 PM|6 PM|7 PM|8 PM|)$/')){
             var weatherID = JSON.stringify(data.hourly[i].weather[0]['id']);
               //Check thunderstorms first, also any bad weather
             weatherType(weatherID);
