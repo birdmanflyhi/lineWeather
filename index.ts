1119b2b8cb4c5f59a9c28abce809d21271ae5225
Matt Crowe <birdman@Matts-MacBook-Pro.local>
2022-11-14 11:38:05 +0100

    updating look of delivered text

--- index.ts
+++ index.ts
@@ -219,9 +219,9 @@ const textEventHandler = async (event: WebhookEvent): Promise<MessageAPIResponse
             } 
             function weatherReply(comment:string){
               response.push(comment +  
-               " Temp:"+ JSON.stringify(data.hourly[i].temp)+ " | humidity:"+ 
-               JSON.stringify(data.hourly[i].humidity)+ " | wind speed:"+
-               JSON.stringify(data.hourly[i].wind_speed)+ " |" + `\n`);
+               " Temp: "+ JSON.stringify(data.hourly[i].temp)+ " | humidity: "+ 
+               JSON.stringify(data.hourly[i].humidity)+ " | wind speed: "+
+               JSON.stringify(data.hourly[i].wind_speed)+ " " + `\n`);
              }
         
     }
